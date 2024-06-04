784788fb1e06a17afda3c52386ae5c8c4f467973
tomu03 <369tomu@gamil.com>
2024-06-04 13:09:27 +0600

    last added on maps activity

--- app/src/main/java/com/example/googlemap/MapsActivity.kt
+++ app/src/main/java/com/example/googlemap/MapsActivity.kt
@@ -13,22 +13,24 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.example.googlemap.databinding.ActivityMapsBinding
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationResult
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.location.Priority
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+class MapsActivity :  AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
-    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
-    private lateinit var locationCallback: LocationCallback
+    private lateinit var fusedLocationProviderClient:FusedLocationProviderClient
+    private lateinit var locationCallback:LocationCallback
 
-
-    companion object {
-        private const val LOCATION_PERMISSION_REQUEST_CODE = 1
+    companion object{
+        private const val LOCATION_PERMISSION_REQUEST_CODE=1
     }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -40,13 +42,11 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
 
-
-
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
-        locationCallback = object : LocationCallback() {
+        locationCallback = object:LocationCallback(){
             override fun onLocationResult(locationResult: LocationResult) {
-                locationResult ?: return
-                for (location in locationResult.locations) {
+                locationResult?:return
+                for (location in locationResult.locations){
                     updateMapLocation(location)
                 }
             }
@@ -54,26 +54,21 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     }
 
     private fun updateMapLocation(location: Location?) {
-        TODO("Not yet implemented")
+        val currentLatLag = LatLng(location?.latitude?:0.0, location?.longitude?:0.0)
+        mMap.addMarker(MarkerOptions().position(currentLatLag).title("My Current Location"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLatLag, 15f))
     }
+
 
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
         enableMyLocation()
-
     }
 
     private fun enableMyLocation() {
-        if (ContextCompat.checkSelfPermission(
-                this,
-                android.Manifest.permission.ACCESS_FINE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            ActivityCompat.requestPermissions(
-                this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
-                LOCATION_PERMISSION_REQUEST_CODE
-            )
-        } else {
+        if(ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_PERMISSION_REQUEST_CODE)
+        }else{
             mMap.isMyLocationEnabled = true
             fusedLocationProviderClient.lastLocation.addOnSuccessListener {
                 it?.let {
@@ -82,10 +77,11 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
             }
             startLocationUpdates()
         }
+
     }
 
     private fun startLocationUpdates() {
-        val locationResult = LocationResult.Builder(Priority.PRIORITY_HIGH_ACCURACY, 100)
+        val locationRequest = LocationRequest.Builder(Priority.PRIORITY_HIGH_ACCURACY, 100)
             .setWaitForAccurateLocation(false)
             .setMinUpdateIntervalMillis(2000)
             .setMaxUpdateDelayMillis(100)
